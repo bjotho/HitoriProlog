787ac1b386521157428f0cf4466de782b2416497
Christoffer Berglund <chrisb14@uia.no>
2017-12-02 14:22:45 +0100

    Added addingVariable method so it will take a List and create a List of lists with a variable and a number

--- testInput.pl
+++ testInput.pl
@@ -9,10 +9,20 @@ sublist(List, Offset, Length, Sublist):-
   length(Sublist, Length),
   append(Sublist, _, Rest).
 
-settingColor([N1,A,N2,B,N3,C,N4,D,
-		      N5,E,N6,F,N7,G,N8,H,
-              N9,I,N10,J,N11,K,N12,L,
-              N13,M,N14,N,N15,O,N16,P]):-
+addingVariable([],C,[]).
+addingVariable([H|T],C,[X|Y]):-
+    append([H,C],[],X),
+    addingVariable(T,C,Y).
+
+choppinglist([],[X|Y]]).
+choppinglist([H1,H2|T],[X|Y]):-
+    append([H1,H2],[],X),
+    choppinglist(T,Y).
+
+settingColor([[N1,A],[N2,B],[N3,C],[N4,D],
+		      [N5,E],[N6,F],[N7,G],[N8,H],
+              [N9,I],[N10,J],[N11,K],[N12,L],
+              [N13,M],[N14,N],[N15,O],[N16,P]]):-
     
     pre(A,B,C,D), pre(E,F,G,H), pre(I,J,K,L), pre(M,N,O,P),
     pre(A,E,I,M), pre(B,F,J,N), pre(C,G,K,O), pre(D,H,L,P).
@@ -24,8 +34,14 @@ pre(A,B,C,D):-
 run:-
     X=[[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4]],
     flatten(X,Flat),
+    addingVariable(Flat,Color,L),
+    write(L).
+    /*
     mergeing(Flat,Color,M),
+    write(M).
+    
     settingColor(M),
-    sublist(M,2,2,P),
-    write(P).
+    choppinglist(M, L),
+    write(L),
+    write(P).*/
 
