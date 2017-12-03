9dc401a255cd8ed05444f030a3da2dc9ca341250
Bjorn-Inge Thoresen <bithor15@uia.no>
2017-12-03 21:01:59 +0100

    Created formatting structure for output

--- HitoriSolver.pl
+++ HitoriSolver.pl
@@ -2,22 +2,32 @@ outputFile('./hitori_solved.txt').
 inputFile('./hitori_unsolved.txt').
 
 
-addingVariable([],[],_).
-addingVariable([H|T],[X|Y],I):-
-    addingRow(H,X,I),
+parsingInput([],[],_).
+parsingInput([H|T],[X|Y],I):-
+    addingSquares(H,X,I),
     length(H,Size),
     I1 is I+Size,
-    addingVariable(T,Y,I1).
+    parsingInput(T,Y,I1).
 
-addingRow([],[],_).
-addingRow([H|T],[X|Y],I):-
+addingSquares([],[],_).
+addingSquares([H|T],[X|Y],I):-
     X=[H,_,I],
     I1 is I+1,
-    addingRow(T,Y,I1).
+    addingSquares(T,Y,I1).
+
+formatOutput([],[]).
+formatOutput([H|T],[X|Y]):-
+    extractRow(H,X),
+    formatOutput(T,Y).
+
+extractRow([],[]).
+extractRow([H|T],[X|Y]):-
+    (H=[V,'W',_] -> X=V ; X='X'),
+    extractRow(T,Y).
 
 twoPlussMany([X,Y|[H|T]]):-
     write('twoPlussMany'),nl,
-    write(X),write(' '),write(Y),write(' '),write(H),write(' '),write(T), nl,
+    write(X),write(' '),write(Y),write(' '),write(H),write(' '),write(T),nl,
     X=Y,
     not(X=H),
     list_to_set(T, S),
@@ -35,10 +45,10 @@ containsDuplicates([H|T]):-
     containsDuplicates(T).
 
 xSomethingX([X,Y,Z|[H|T]]):-
-    (X=Z,Y=[_,'White']);
+    (X=Z,Y=[_,'W']);
     xSomethingX([Y,Z,H|T]).
 xSomethingX([X,Y,X]):-
-    Y=[_,'White'].
+    Y=[_,'W'].
 
 checkTests([]).
 checkTests([H|T]):-
@@ -54,13 +64,22 @@ settingColor([[N1,A],[N2,B],[N3,C],[N4,D],
 
 
 color(A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P):-
-    member('Black',[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P]),
-    member('White',[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P]).
+    member('B',[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P]),
+    member('W',[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P]).
 
 
 doSolve(SizeX,SizeY,Input,Output):-
-    addingVariable(Input,SquareList,1),
-    write(SquareList).
+    /*parsingInput takes in a list (Input), and binds SquareList to a list of squares. Final parameter is the starting index*/
+    %parsingInput(Input,SquareList,1),
+    %write(SquareList).
+
+    /*formatOutput takes in a solved hitori puzzle list and generates output in the desired format*/
+    ExampleSolved=[[[2,'B',1],[2,'W',2],[2,'B',3],[4,'W',4]],
+                   [[1,'W',5],[4,'W',6],[2,'W',7],[3,'W',8]],
+                   [[2,'W',9],[3,'W',10],[2,'B',11],[1,'W',12]],
+                   [[3,'W',13],[4,'B',14],[1,'W',15],[2,'W',16]]],
+    formatOutput(ExampleSolved,Output),
+    print(Output).
     
     %addingRow(L,SquareList),
     %checkTests(SquareList),
