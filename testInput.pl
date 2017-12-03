285520fabd1dc66a1c8b813e856e8a6fd10315be
Christoffer Berglund <chrisb14@uia.no>
2017-12-04 00:11:34 +0100

    Startet on row checking

--- testInput.pl
+++ testInput.pl
@@ -17,27 +17,38 @@ addColor([H|T]):-
     member('White',H),
     addColor(T).
 
-checkColor([],[]).
-checkColor([H1,H2|T],[X1,X2|Y]):-
-    H2='Black',X2='White';
+checkColor([],[],5).
+checkColor([H1,H2,H3|T],[X1,X2,X3|Y],5):-
+    /*Check rows*/
+    H1=X1,
+    (H2='Black',X2='White';
     H2='White',X2='Black';
-    H2='White',H2=X2.
+    H2='White',H2=X2);
+    not(H1=X1),
+    (H2='Black',X2='White';
+    H2='White',X2='Black';
+    H2='White',H2=X2).
+    /*Check columns*/
 
+checkRow([]).
+checkRow([[H|R]|T]):-
+    member(H,T),
+    checkRow([R|T]).
+    
 getSquares([X]).
 getSquares([H1,H2|T]):-
-    checkColor(H1,H2),
+    checkColor(H1,H2,5),
     %checkNumbers(H1,H2),
     getSquares([H2|T]).
 
 getRow([]).
 getRow([H|T]):-
     addColor(H),
-    getSquares(H),
-    getRow(T).
+    checkRow(H).
+    %getRow(T).
 
 checkAll(List):-
-    getRow(List),
-    write(List).
+    getRow(List).
 
 run:-
     X=[[1,3,3,5,5],[4,1,5,3,2],[2,1,1,3,3],[5,3,4,1,4],[3,3,4,5,4]],
