4e805e71ab4edf9b27c7c8ad808fd5a3c19fd439
Bjorn Inge Thoresen <bithor15@uia.no>
2017-12-04 10:44:23 +0100

    xSomethingX working

--- HitoriSolver.pl
+++ HitoriSolver.pl
@@ -50,18 +50,20 @@ containsDuplicates([H|T]):-
     containsDuplicates(T).
 */
 
+xSomethingX([X,Y,Z|_]):-
+	getHead(X,S1),
+	getHead(Z,S2),
+    S1=S2,Y=[_,'W',_].
 xSomethingX([X,Y,Z|[H|T]]):-
-    getHead(X,S1),
-    getHead(Z,S2),
-    S1=S2,Y=[_,'W',_],
     xSomethingX([Y,Z,H|T]).
-xSomethingX([X,Y,X]|_):-
-    Y=[_,'W',_].
+
 
 checkTests([]).
 checkTests([H|T]):-
-    xSomethingX(H);
-    checkTests(T).
+    (xSomethingX(H),
+    checkTests(T));
+    (xSomethingX(H);
+    checkTests(T)).
 
 
 doSolve(SizeX,SizeY,Input,Output):-
@@ -81,7 +83,6 @@ doSolve(SizeX,SizeY,Input,Output):-
                    [[1,'W',5],[4,'W',6],[2,'W',7],[3,'W',8]],
                    [[2,'W',9],[3,'W',10],[2,'B',11],[1,'W',12]],
                    [[3,'W',13],[4,'B',14],[1,'W',15],[2,'W',16]]],
-                   
 
 /*doSolve(5,_,_,[[1,'X',3,'X',5],[4,1,5,3,2],[2,'X',1,'X',3],[5,3,'X',1,4],[3,'X',4,5,'X']]):-!.
 doSolve(7,_,_,[['X',4,1,'X',6,5,'X'],[6,'X',3,5,'X',1,4],[5,3,'X',1,2,'X',6],['X',7,6,'X',1,2,5],[4,'X',7,2,'X',6,'X'],[1,6,2,7,5,4,3],[7,'X',5,'X',4,'X',2]]):-!.*/
